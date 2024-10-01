df615b7876b6d1849ae86ec67a77293f8fd74dd2
torikallcode <torikalcontent@gmail.com>
2024-10-01 19:05:35 +0800

    add module Addition

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/torikallcode/app_matematika
+
+go 1.22.5
+
+require github.com/torikallcode/module-matematika v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	Additon "github.com/torikallcode/module-matematika"
+)
+
+func main() {
+	tambah := Additon.Addition(2, 3)
+	fmt.Println(tambah)
+
+}
